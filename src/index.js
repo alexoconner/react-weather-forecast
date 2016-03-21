eefcc69d30f5324c828a0fc5125e6a1832ccfd19
Alexander Skrabl <alexander@skrabl.de>
2016-03-21 22:46:06 +0000

    some initial set up

---
+++ src/index.js
@@ -0,0 +1,7 @@
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import WeatherWidget from './weather-widget.jsx';
+
+ReactDOM.render( <WeatherWidget />, document.getElementById('App') );
---
+++ src/weather-widget.jsx
@@ -0,0 +1,17 @@
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class WeatherWidget extends React.Component {
+    constructor( props ) {
+        super( props );
+    }
+
+    render() {
+        return (
+            <h1>Weather Widget</h1>
+        )
+    }
+}
+
+export default WeatherWidget;
